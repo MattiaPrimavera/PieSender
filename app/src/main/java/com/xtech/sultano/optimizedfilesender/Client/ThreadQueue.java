ee9496d9f115709ac9fcead46094f3bb63af4a1a
Mattia Primavera <sconqua@gmail.com>
2016-11-28 15:11:28 +0000

    Mutual Exclusion handling on ThreadQueue

--- app/src/main/java/com/xtech/sultano/optimizedfilesender/Client/ThreadQueue.java
+++ app/src/main/java/com/xtech/sultano/optimizedfilesender/Client/ThreadQueue.java
@@ -4,61 +4,53 @@ import java.util.LinkedList;
 
 public class ThreadQueue {
     private final int MAX_ACTIVE_THREADS = 5;
+    private int activeThreads;
     private LinkedList<Thread> list;
 
     // Queue constructor
     public ThreadQueue()
     {
+        this.activeThreads = 0;
         // Create a new LinkedList.
         list = new LinkedList<Thread>();
     }
 
-    public boolean isEmpty()
-    // Post: Returns true if the queue is empty. Otherwise, false.
-    {
+    public boolean isEmpty(){
         return (list.size() == 0);
     }
 
-    public void enqueue(Thread item)
-    // Post: An item is added to the back of the queue.
-    {
-        if(list.size() <= MAX_ACTIVE_THREADS){
-            item.start();
-        }
+    public synchronized void enqueue(Thread item){
         // Append the item to the end of our linked list.
         list.add(item);
+        if(this.activeThreads <= MAX_ACTIVE_THREADS){
+            this.dequeue();
+        }
+
     }
 
-    public Thread dequeue()
-    // Pre: this.isEmpty() == false
-    // Post: The item at the front of the queue is returned and
-    //         deleted from the queue. Returns null if precondition
-    //         not met.
-    {
-        // Store a reference to the item at the front of the queue
-        //   so that it does not get garbage collected when we
-        //   remove it from the list.
-        // Note: list.get(...) returns null if item not found at
-        //   specified index. See postcondition.
-        Thread item = list.get(1);
-        // Remove the item from the list.
-        // My implementation of the linked list is based on the
-        //   J2SE API reference. In both, elements start at 1,
-        //   unlike arrays which start at 0.
-        list.remove(1);
-
-        // Return the item
-        return item;
+    public synchronized void increaseActiveThreads(){
+        this.activeThreads++;
     }
 
-    public Thread peek()
-    // Pre: this.isEmpty() == false
-    // Post: The item at the front of the queue is returned and
-    //         deleted from the queue. Returns null if precondition
-    //         not met.
-    {
-        // This method is very similar to dequeue().
-        // See Queue.dequeue() for comments.
-        return list.get(1);
+    public synchronized void decreaseActiveThreads(){
+        this.activeThreads--;
+    }
+
+    public synchronized Thread dequeue(){
+        if(!this.isEmpty()){
+            this.increaseActiveThreads();
+            Thread item = list.get(0);
+            item.start();
+            list.remove(0);
+            return item;
+
+        }
+        return null;
+    }
+
+    public Thread peek(){
+        if(!isEmpty())
+            return list.get(1);
+        return null;
     }
 }
